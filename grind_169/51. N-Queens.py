af43556d223a2a9ef6ac79da65ab6204d3368a24
ted li <ated0963@gmail.com>
2023-12-06 04:44:41 +0800

    update G169

--- grind_169/51. N-Queens.py
+++ grind_169/51. N-Queens.py
@@ -4,6 +4,7 @@ class Solution:
         ans = []
         col = [0] * n
         on_path, diag1, diag2 = [False] * n, [False] * m, [False] * m
+
         def dfs(r: int) -> None:
             if r == n:
                 ans.append(['.' * c + 'Q' + '.' * (n - 1 - c) for c in col])
@@ -14,10 +15,9 @@ class Solution:
                     on_path[c] = diag1[r + c] = diag2[r - c] = True
                     dfs(r + 1)
                     on_path[c] = diag1[r + c] = diag2[r - c] = False  # 恢复现场
+
         dfs(0)
         return ans
 
-# 作者：灵茶山艾府
-# 链接：https://leetcode.cn/problems/n-queens/solutions/2079586/hui-su-tao-lu-miao-sha-nhuang-hou-shi-pi-mljv/
-# 来源：力扣（LeetCode）
-# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+# by @0x3f https://leetcode.cn/problems/n-queens/solutions/2079586/hui-su-tao-lu-miao-sha-nhuang-hou-shi-pi-mljv/
+# ref = https://leetcode.cn/problems/n-queens/solutions/398929/nhuang-hou-by-leetcode-solution/
